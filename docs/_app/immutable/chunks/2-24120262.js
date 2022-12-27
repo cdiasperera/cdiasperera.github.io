7bd5f27052956ba8cc03c4a1c8367b4d7d090ce1
channa <cdiasperera@gmail.com>
2022-12-27 23:13:24 +0100

    deploy

---
+++ docs/_app/immutable/chunks/2-24120262.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-a89d5d6b.js";export{t as component};
--- docs/_app/immutable/chunks/2-412b2d2e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-9d326c1d.js";export{t as component};
